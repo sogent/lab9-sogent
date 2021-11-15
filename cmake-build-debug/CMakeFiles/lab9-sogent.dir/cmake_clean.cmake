53ee828ee1ea3a0aceb325f4c08478f098be575a
soge-commits <sogent@mail.umkc.edu>
2021-11-14 18:42:42 -0600

    program complete

---
+++ cmake-build-debug/CMakeFiles/lab9-sogent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab9-sogent.dir/Fraction.cpp.o"
+  "CMakeFiles/lab9-sogent.dir/Lab10.cpp.o"
+  "lab9-sogent"
+  "lab9-sogent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab9-sogent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
